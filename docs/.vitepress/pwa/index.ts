075bd3e7ecb1325f10f50881061939bbdd59c924
whitekite <1075790909@qq.com>
2024-05-03 17:56:47 +0800

    docs: fix pwa manifest setting error

--- docs/.vitepress/pwa/index.ts
+++ docs/.vitepress/pwa/index.ts
@@ -3,9 +3,9 @@ import { icons } from './icons'
 
 export const pwa: PwaOptions = {
   manifest: {
-    name: 'Vrx Arco',
-    short_name: 'Vrx Arco',
-    description: '对arco-design-vue 的pro预设的封装',
+    name: 'Vrx Color Picker Kit',
+    short_name: 'Vrx Color Picker',
+    description: '用于快速组装HSV颜色选择器的工具',
     theme_color: '#43E97B',
     icons,
   },
